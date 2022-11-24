1d7398f21179f626a853318031ee2e64c7352579
BurningInfern0 <burninginfern0@outlook.com>
2022-11-23 20:24:30 -0500

    big thing
    - the ability to register proxies (name and brackets)
    - set proxy avatars
    - send messages as proxies

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import csv
 from discord.ext import commands
 from discord import Webhook
 
@@ -46,37 +47,90 @@ async def about(ctx):
     if modified == True:
         embedVar.add_field(name="Warning:", value="*The owner of this bot has enabled the modified variable, which means this bot's code has been modified and put up for public use. A valid respository link for this code will be provided at the bottom.*", inline=False)
     embedVar.set_footer(text="Created by BurningInfern0.")
+    embedVar.set_image(url="https://user-images.githubusercontent.com/74492478/203663460-6863d8e6-66d8-4379-8fe9-aba48de15262.png")
     embedVar.add_field(name="Did you know the bot is open source?", value="That means **anyone** can view the source code, or how the bot works. You can change/add/remove what you want, and self host your own Uranium instance. But remember, if you distribute your personal code, you **must** follow the terms and conditions of the GNU Affero General Public Licence v3.", inline=False)
     embedVar.add_field(name="GNU Affero General Public License v3", value="https://www.gnu.org/licenses/agpl-3.0.html", inline=False)
     embedVar.add_field(name="Repository Link", value=respositoryLink, inline=False)
     await ctx.send(embed=embedVar)
 
 # all this does is create an empty CSV file for storing user proxy information.
-@uranium.command()
+@uranium.group(invoke_without_command=True)
 async def init(ctx):
     databasePath = "./user-data/{0}.csv".format(ctx.message.author.id)
     databaseExists = os.path.exists(databasePath)
     if databaseExists == True:
-        await ctx.send(":x: *Your database is already initialized, and exists.*")
+        await ctx.send(":x: *Your database is already initialized, and exists.*\nIf you would like to initialize it again, **deleting all of your proxies and settings**, please do `u.init confirm`.")
     else:
         databaseCreation = open(databasePath, "w")
-        databaseCreation.write("testing")
         databaseCreation.close()
         await ctx.send(":white_check_mark: *Your database has been created and initialized. You can now create proxies.*")
 
-@uranium.group()
-async def wh(ctx):
-    pass
+@init.command()
+async def confirm(ctx):
+    databasePath = "./user-data/{0}.csv".format(ctx.message.author.id)
+    os.remove(databasePath)
+    databaseCreation = open(databasePath, "w")
+    databaseCreation.close()
+    await ctx.send(":white_check_mark: *Your database has been reinitalized. All former proxies and settings have been deleted.*")
+    
 
-@wh.command(description="mainly for debugging", aliases=["p"])
-async def present(ctx):
+@uranium.group(invoke_without_command=True, aliases=["p"])
+async def proxy(ctx):
+    await ctx.send(":x: *Please provide a subcommand.*\nWhat do you want me to do with proxies? See `u.help proxy` for more information.")
+
+@proxy.command(description="register proxy")
+async def register(ctx, name:str, brackets:str):
+    appendProxy = open("./user-data/{0}.csv".format(ctx.message.author.id), "a")
+    appendProxy.write("{0},{1},*\n".format(brackets, name))
+    appendProxy.close()
+    await ctx.send("Wonderful! `{0}` has been created under your user data using the brackets `{1}`.\nTo send a message via this proxy, send `u.proxy send {1} I'm a proxy!`".format(name, brackets))
+
+@proxy.command(description="set a proxy's avatar")
+async def avatar(ctx, name:str):
+    try:
+        avatar = ctx.message.attachments[0]
+    except:
+        await ctx.send(":x: *No image was provided.*")
+        return
+    
+    global editedLine
+    global line
+    editedLine = 0
+    line = ""
+
+    try:
+        with open("./user-data/{0}.csv".format(ctx.message.author.id)) as f:
+            csv_reader = csv.reader(f, delimiter=',')
+            for line in csv_reader:
+                if line[1] == name:
+                    break
+                editedLine = editedLine + 1
+            else:
+                raise EOFError()
+        f = open("./user-data/{0}.csv".format(ctx.message.author.id), 'r')
+        filesaver = f.readlines()
+        filesaver[editedLine] = "{0},{1},{2}\n".format(line[0], line[1], avatar.url)
+        f.close()
+
+        x = open("./user-data/{0}.csv".format(ctx.message.author.id), 'w')
+        x.writelines(filesaver)
+        x.close()
+
+        await ctx.send(":white_check_mark: *Avatar successfully saved.*\nPlease note, if the image you specified gets deleted, then your proxy's avatar will no longer work.")
+    except IOError:
+        await ctx.send(":x: *I could not find your user database file. Has it been initialized?*")
+    except EOFError:
+        await ctx.send(":x: *I could not find a proxy with that name in your user database file. Has it been registered yet?*")
+
+@proxy.command(description="mainly for debugging", aliases=["whs"])
+async def webhookstatus(ctx):
     direc = os.listdir("./webhook-data/")
     for file in direc:
         if str(ctx.channel.id) in file:
             webhookRead = open("./webhook-data/{0}".format(str(ctx.channel.id), "r"))
             webhookID = webhookRead.read()
             webhookRead.close()
-            await ctx.send(":white_check_mark: Webhook ID of {0} for this channel is present in my local database.".format(str(webhookID)))
+            await ctx.send(":white_check_mark: Webhook ID of `{0}` for this channel is present in my local database.".format(str(webhookID)))
             break
     else:
         await ctx.send(":x: I do not have a local webhook database entry for this channel.")
@@ -90,8 +144,8 @@ async def present(ctx):
     except:
         await ctx.send(":x: My local webhook database did not find a match in this channel's webhook list.")
 
-@wh.command(aliases=["s"])
-async def send(ctx, name: str, *, msg):
+@proxy.command(aliases=["s"])
+async def send(ctx, brackets:str, *, msg):
     # check if channel webhook exists in local database
     direc = os.listdir("./webhook-data/")
     for file in direc:
@@ -111,7 +165,18 @@ async def send(ctx, name: str, *, msg):
     webhookList = await ctx.message.channel.webhooks()
     for wh in webhookList:
         if str(webhookID) == str(wh):
-            await wh.send(msg, username=name)
+            with open("./user-data/{0}.csv".format(ctx.message.author.id)) as f:
+                csv_reader = csv.reader(f, delimiter=',')
+                for line in csv_reader:
+                    if line[0] == brackets:
+                        name = line[1]
+                        avtr = line[2]
+            await ctx.message.delete()
+            if avtr == "*":
+                await wh.send(msg, username=name)
+            else:
+                await wh.send(msg, username=name, avatar_url=avtr)
+            
             return
     await ctx.send(":x: **Something went wrong!**\nThe webhook ID in my local database could not be found in this channel's webhook list. *Did the webhook get deleted?*")
 
